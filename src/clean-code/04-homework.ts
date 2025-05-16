15c3b0cd646b16ec6ad5b51b8f589b452fe8c72e
Gerardo Gael Rojas Ramos <gerardo.rojasramos@ucr.ac.cr>
2025-05-16 14:52:36 -0600

    Realizado de clean code

--- src/clean-code/04-homework.ts
+++ src/clean-code/04-homework.ts
@@ -5,26 +5,31 @@
     // includes? arrays?
     function isRedFruit( fruit: string ): boolean {
         
-        if ( fruit === 'manzana' || fruit === 'cereza' || fruit === 'ciruela' ) {
-            return true;
-        } else {
-            return false;
-        }
+        const fruits = ['fresa', 'cereza', 'ciruela'];
+        return fruits.includes(fruit) ? true : false
     }
 
     // Simplificar esta función
     // switch? Object literal? validar posibles colores
     function getFruitsByColor( color: string ): string[] {
 
-        if ( color === 'red' ) {
-            return ['manzana','fresa'];
-        } else if ( color === 'yellow') {
-            return ['piña','banana'];
-        } else if ( color === 'purple') {
-            return ['moras','uvas']
-        } else {
-            throw Error('the color must be: red, yellow, purple');
+        switch(color) {
+            case 'red' : 
+                return ['manzana', 'fresa'];
+
+            case 'yellow':
+                return ['piña', 'banana']
+
+            case 'purple':
+                return ['moras', 'uvas']
+
+            default:
+                throw Error('the color must be: red, yellow, purple')
+
         }
+
+
+
     }
 
     // Simplificar esta función
@@ -34,30 +39,16 @@
     let isFourthStepWorking = true;
 
     function workingSteps() {
-        if( isFirstStepWorking === true ) {
-            if( isSecondStepWorking === true ) {
-                if( isThirdStepWorking === true ) {
-                    if( isFourthStepWorking === true ) {
-                        return 'Working properly!';
-                    }
-                    else {
-                        return 'Fourth step broken.';
-                    }
-                }
-                else {
-                    return 'Third step broken.';
-                }
-            }
-            else {
-                return 'Second step broken.';
-            }
-        }
-        else {
-            return 'First step broken.';
-        }
+        
+        if (!isFirstStepWorking) return 'First step broken.'
+        if (!isSecondStepWorking) return 'Second step broken.'
+        if (!isThirdStepWorking) return 'Third step broken'
+        if (!isFourthStepWorking) return 'Fourth step broken'
+        return 'Working properly!'
+        
+    
     }
-
-
+        
     // isRedFruit
     console.log({ isRedFruit: isRedFruit('cereza'), fruit: 'cereza' }); // true
     console.log({ isRedFruit: isRedFruit('piña'), fruit: 'piña' }); // true
